9fab77e29e5de969c8cdd984da6f04538e2a95a7
Opilane TTHK <opilane@tthk.ee>
2026-03-19 13:47:52 +0200

    enemy

---
+++ KordamisYlesanded-main/KordamisYlesanded-main/CLASS_ENUM_STRUCT/Adventure/Boss.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class Boss : Enemy
+{
+
+}
---
+++ KordamisYlesanded-main/KordamisYlesanded-main/CLASS_ENUM_STRUCT/Adventure/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/* Lisa eventsystemile juurde uus event "enemyduel"
+Lisa juurde kaks uut klassi, baasklass enemy ja enemyl baseeruv klass Boss
+enemyl on omadusteks nimi (string), health (int), elud on enemy puhul algväärtusega 1, bopssil võib rohkem olla (int),
+ja catchphrase (string) ning hitpower (int)
+boss omab ka andmevälja "BossWeaponName" (string) ja "BossWeaponHitPower" (int)
+
+eventsystemys enemyduel event teostab eventi sees tsüklis kakluse mis kestab
+niikaua kuni kas mängijal on elud otsas või enemyl on elud otsas.
+juhuarvuga otsustatakse kas vastaseks on üks kolmest enemy objektist (genereeritakse program.csis) või boss.
+bossi puhul juhuarvuga otsustatakse kas löök on mööda, boss lööb relvaga või boss lööb käega, tavalise enemy puhul ainult mööda või käega.
+kui boss sureb, on mängijal võimalus relv üles võtta.
+*/
+
+public class Enemy
+{
+	public Enemy(string name, int health, int lives, string catchphrase, int hitpower)
+	{
+		Name = name;
+		Health = health;
+		Lives = lives;
+		Catchphrase = catchphrase;
+		Hitpower = hitpower;
+	}
+	public string Name { get; set; }
+	public int Health { get; set; } = 100;
+	public int Lives { get; set; } = 1;
+	public string Catchphrase { get; set; }
+	public int Hitpower { get; set; }
+}
